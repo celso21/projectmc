6f31d3abc06e4c353a0f934f2cf685823795ca5e
celso21 <celso.aquino@ymail.com>
2019-02-01 22:42:09 -0200

    Projeto criado

---
+++ src/main/java/com/celsoaquino/projectmc/ProjectmcApplication.java
@@ -0,0 +1,14 @@
+package com.celsoaquino.projectmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmcApplication.class, args);
+	}
+
+}
+
