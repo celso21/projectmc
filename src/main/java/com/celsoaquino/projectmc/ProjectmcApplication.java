b8e91cd8e4b6c3d464ede89f322f987d06488e6e
celso21 <celso.aquino@ymail.com>
2019-02-02 21:19:29 -0200

    Criando operação de instaciação

--- src/main/java/com/celsoaquino/projectmc/ProjectmcApplication.java
+++ src/main/java/com/celsoaquino/projectmc/ProjectmcApplication.java
@@ -1,13 +1,32 @@
 package com.celsoaquino.projectmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectmcApplication {
+import com.celsoaquino.projectmc.domain.Categoria;
+import com.celsoaquino.projectmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjectmcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectmcApplication.class, args);
+		SpringApplication.run(ProjectmcApplication.class, args);		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática"); 
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
